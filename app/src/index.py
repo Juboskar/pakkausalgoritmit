6dcd17aa3a4028a455f3782cb206f0e0e8ab2930
Oskari-Juvakka <oskari.juvakka@gmail.com>
2022-01-27 13:40:04 +0200

    made ui

--- app/src/index.py
+++ app/src/index.py
@@ -1,9 +1,13 @@
 from ui.ui import UI
+from tkinter import Tk
 
 
 def main():
-    ui = UI()
+    window = Tk()
+    window.title('Pakkaamo')
+    ui = UI(window)
     ui.start()
+    window.mainloop()
 
 
 if __name__ == "__main__":
--- app/src/ui/ui.py
+++ app/src/ui/ui.py
@@ -1,10 +1,18 @@
-# todo: tänne tulee ui palikat, joilla käytetään compressing_serviceä
+from tkinter import Tk, ttk, filedialog
+
 
 class UI:
     "käyttöliittymän rakentava koodi"
 
-    def __init__(self):
-        pass
+    def __init__(self, root):
+        self._root = root
+        self.filename = None
 
     def start(self):
-        pass
+        label = ttk.Label(master=self._root, text="Valitse pakattava tiedosto")
+        button = ttk.Button(self._root, text='Open', command=self.upload_action)
+        label.pack()
+        button.pack()
+
+    def upload_action(self, event=None):
+        self.filename = filedialog.askopenfilename()
