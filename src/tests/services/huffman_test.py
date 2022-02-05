e257ad97fa7d9dabcfdb8feee69f345144446b7f
Oskari-Juvakka <oskari.juvakka@gmail.com>
2022-02-05 16:42:19 +0200

    test fix

--- src/tests/services/huffman_test.py
+++ src/tests/services/huffman_test.py
@@ -5,7 +5,7 @@ from services.huffman import HuffmanAlgorithm
 class TestHuffman(unittest.TestCase):
     def test_tree_is_correct(self):
         huffman = HuffmanAlgorithm()
-        huffman.build_tree("ABCABCAAABBAAACCCC")
+        huffman.build_tree("AAAAABCCCDD")
         self.assertEqual(str(huffman.tree),
                          "Node: None: 11, (Node: A: 5, (None, None), " +
                          "Node: None: 6, (Node: C: 3, (None, None), " +
@@ -14,6 +14,6 @@ class TestHuffman(unittest.TestCase):
 
     def test_bit_values_are_calculated_correctly(self):
         huffman = HuffmanAlgorithm()
-        huffman.build_tree("ABCABCAAABBAAACCCC")
+        huffman.build_tree("AAAAABCCCDD")
         self.assertEqual(huffman.bit_values(),
                          {'D': '000', 'B': '001', 'C': '01', 'A': '1'})
