187d7d23800ba771f141aa6dab53060945e437be
Oskari-Juvakka <oskari.juvakka@gmail.com>
2022-02-14 17:52:34 +0200

    work with lempel-ziv

--- src/services/lempel_ziv.py
+++ src/services/lempel_ziv.py
@@ -9,6 +9,26 @@ class LzAlgorithm:
 
     def compress(self, string: str):
         "pakkaa lempel-ziv algoritmilla"
+        string_dict = {c: list(set(string)).index(c) for c in set(string)}
+        print(string_dict)
+
+        string_dict = {'a':0, 'b':1, 'd':2, 'n':3, '_':4}
+
+        output = []
+        s = ''
+        for c in string:
+            if s + c in string_dict.keys():
+                s += c
+            else:
+                output.append(string_dict[s])
+                string_dict[s + c] = len(string_dict)
+                s = c
+        output.append(string_dict[s])
+        print(string_dict)
+        print(output)
 
     def decompress(self, string: str):
         "purkaa lempel-ziv algoritmilla pakatun tekstin"
+
+
+
